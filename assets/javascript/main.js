4cbd50ca5e0a639c7897e722ff1c2ca69140ab1f
matteopoliti <matteo.politi17@gmail.com>
2024-01-08 18:17:00 +0100

    feat(main): add html style

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -10,8 +10,21 @@ const risultatoHtml = document.getElementById("risultato");
 
 let prezzoBiglietto = costoKm * kmPercorsi;
 
+let eta = document.getElementById("mySelect");
+
 buttonHtml.addEventListener( "click", function(){
 
-    console.log(nomePersona.value, kmPercorsi.value, costoKm, prezzoBiglietto)
+    console.log(nomePersona.value, kmPercorsi.value, costoKm, prezzoBiglietto, eta.value)
+    if( eta.value == minorenne){
+        prezzoBiglietto = prezzoBiglietto * 0.8
+        risultatoHtml.innerHTML = `il prezzo è ${prezzoBiglietto}`
+    }
+    else if( eta.value == over){
+        prezzoBiglietto = prezzoBiglietto * 0.4
+        risultatoHtml.innerHTML = `il prezzo è ${prezzoBiglietto}`
+    }
+    else {
+        risultatoHtml.innerHTML = `il prezzo è ${prezzoBiglietto}`
+    }
 
 } )
