225a0466ce2f4cd541e4a56a6b5bed999f1804ed
matteopoliti <matteo.politi17@gmail.com>
2024-01-09 12:52:26 +0100

    fix(javascript): fix javascript code

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,30 +1,57 @@
-let nomePersona = document.getElementById("name");
-
-let kmPercorsi = document.getElementById("km");
-
+const nomePersona = document.getElementById("name");
+const kmPercorsi = document.getElementById("km");
+const eta = document.getElementById("mySelect");
+const buttonHtml = document.getElementById("button");
+const carrozzaHtml = document.getElementById("carrozza");
+const codiceCpHtml = document.getElementById("codPostale");
+const risultatoNomeCognomeHtml = document.getElementById("risultatoNomeCognome");
+const risultatoBigliettoHtml = document.getElementById("biglietto");
+const risultatoPrezzoHtml = document.getElementById("risultato")
 const costoKm = 0.21;
 
-const buttonHtml = document.getElementById("button");
 
-const risultatoHtml = document.getElementById("risultato");
-
-let prezzoBiglietto = costoKm * kmPercorsi;
-
-let eta = document.getElementById("mySelect");
 
 buttonHtml.addEventListener( "click", function(){
+    document.querySelector("h2").classList.remove("d-none")
+    document.querySelector("div.d-none").classList.remove("d-none")
 
-    console.log(nomePersona.value, kmPercorsi.value, costoKm, prezzoBiglietto, eta.value)
-    if( eta.value == minorenne){
-        prezzoBiglietto = prezzoBiglietto * 0.8
-        risultatoHtml.innerHTML = `il prezzo è ${prezzoBiglietto}`
+    let codiceCarrozza = RandomNumber( 0, 10)
+    let codiceCp = RandomNumber( 100, 10000)
+
+    carrozzaHtml.innerHTML = codiceCarrozza
+    codiceCpHtml.innerHTML = codiceCp
+
+    risultatoNomeCognomeHtml.innerHTML = nomePersona.value
+
+    if( eta.value === "minorenne"){
+        
+        risultatoBigliettoHtml.innerHTML = 'Biglietto under 18'
+
+        risultatoPrezzoHtml.innerHTML = ((kmPercorsi.value * costoKm) * 0.8).toFixed(2) + "€"
     }
-    else if( eta.value == over){
-        prezzoBiglietto = prezzoBiglietto * 0.4
-        risultatoHtml.innerHTML = `il prezzo è ${prezzoBiglietto}`
+    else if( eta.value === "maggiorenne"){
+        
+        risultatoBigliettoHtml.innerHTML = 'Biglietto standard'
+
+        risultatoPrezzoHtml.innerHTML = (kmPercorsi.value * costoKm).toFixed(2) + "€"
     }
-    else {
-        risultatoHtml.innerHTML = `il prezzo è ${prezzoBiglietto}`
+    else if( eta.value === "over") {
+        risultatoBigliettoHtml.innerHTML = 'Biglietto over 65'
+
+        risultatoPrezzoHtml.innerHTML = ((kmPercorsi.value * costoKm) * 0.6).toFixed(2) + "€"
     }
+    
 
 } )
+
+document.getElementById("ricaricaButton").addEventListener("click", ricaricaPagina);
+
+
+
+function RandomNumber (min, max){
+    return Math.floor( Math.random()*(max - min + 1) + min)
+}
+
+function ricaricaPagina() {
+    location.reload();
+}
